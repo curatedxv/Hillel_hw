5c3464f03c992c8280f3a2de48665eaa2bf83561
vasiliy <vasiliyalex2008@gmail.com>
2024-11-04 12:20:08 +0200

    hw2

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, render_template
+from sqlite3 import connect
 
+from flask import Flask, request, render_template, redirect
+import sqlite3
 app = Flask(__name__)
 '''
 /login [GET, POST]
@@ -34,6 +36,24 @@ app = Flask(__name__)
 
 /compare [GET, PUT]
 '''
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+class Database(object):
+    def __init__(self, filename):
+        self.con = sqlite3.connect('db1.db')
+        self.con.row_factory = dict_factory
+        self.cur = self.con.cursor()
+    def __enter__(self):
+        return self.cur
+    def __exit__(self, type, value, traceback):
+        self.con.commit()
+        self.con.close()
+
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
@@ -50,7 +70,13 @@ def register():
     if request.method == 'GET':
         return render_template('register.html')
     if request.method == 'POST':
-        return 'POST'
+        with Database('db1.db') as cur:
+            form_data = request.form
+            cur.execute('''INSERT INTO user 
+            (login, password, ipn, full_name, contacs, photo, passport) 
+            VALUES (?, ?, ?, ?, ?, ?, ?)''',
+  (form_data['login'], form_data['password'], form_data['ipn'], form_data['full_name'], form_data['contacts'], form_data['photo'], form_data['passport']))
+        return redirect('/')
 
 @app.route('/logout', methods=['GET', 'POST', 'DELETE'])
 def logout():
@@ -64,7 +90,7 @@ def logout():
 @app.route('/profile', methods=['GET', 'DELETE'])
 def profile():
     if request.method == 'GET':
-        return 'GET'
+        return render_template('user.html')
     if request.method == 'DELETE':
         return 'DELETE'
 
@@ -93,9 +119,18 @@ def search_history():
 @app.route('/items',  methods=['GET', 'POST'])
 def items():
     if request.method == 'GET':
-        return 'GET'
+        with Database('db1.db') as cur:
+            cur.execute('SELECT * FROM item')
+            items = cur.fetchall()
+            return render_template('items.html', items=items)
     if request.method == 'POST':
-        return 'POST'
+        with Database('db1.db') as cur:
+            form_data = request.form
+            cur.execute('''INSERT INTO item
+            (photo, name, description, price_hour, price_day, price_week, price_month)
+            VALUES (?, ?, ?, ?, ?, ?, ?)''',
+  (form_data['photo'], form_data['name'], form_data['description'], form_data['price_hour'], form_data['price_day'], form_data['price_week'], form_data['price_months']))
+            return redirect('/items')
 
 @app.route('/items/<item_id>', methods=['GET', 'DELETE'])
 def item(item_id):
@@ -136,8 +171,10 @@ def search():
     if request.method == 'POST':
         return 'POST'
 
-@app.route('/complain', methods=['POST'])
+@app.route('/complain', methods=['GET', 'POST'])
 def complain():
+    if request.method == 'GET':
+        return render_template('complain.html')
     if request.method == 'POST':
         return 'POST'
 
